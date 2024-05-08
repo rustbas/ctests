e650833a016eaa39bbfe15bafebda4b05a34d30f
Basyrov Rustam <bronit10@morozko.ru>
2024-05-08 16:53:50 +0300

    init point struct for testings and example

--- main.c
+++ main.c
@@ -6,6 +6,25 @@
 
 int some_f(int a, int b) { return a + b; }
 
+typedef struct {
+  int x, y;
+} Point;
+
+int compare_point(void *p1, void *p2) {
+  Point *tp1 = (Point *)p1;
+  Point *tp2 = (Point *)p2;
+
+  if ((tp1->x == tp2->x) && (tp1->y == tp2->y)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+Point p1 = {.x = 2, .y = 1};
+Point p2 = {.x = 1, .y = 2};
+Point p3 = {.x = 2, .y = 1};
+
 int main() {
 
   CT_ASSERT_INT(some_f(1, 2), 3);
@@ -16,7 +35,7 @@ int main() {
 
   CT_STAT();
 
-  printf("Hello, %d\n", some_f(1, 2));
+  printf("Hello, %s\n", compare_point(&p1, &p3) ? "true" : "false");
 
   return 0;
 }
